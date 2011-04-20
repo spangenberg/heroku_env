31fdd4bfa66d1d77d4275c121005770b62e1870b
Daniel Spangenberg <daniel.spangenberg@parcydo.com>
2011-04-20 05:50:57 +0200

    Improved starting time and added rake task for creating heroku_env.yml

--- lib/heroku_env.rb
+++ lib/heroku_env.rb
@@ -1,16 +1,31 @@
 module HerokuEnv
   module Rails
     class Railtie < ::Rails::Railtie
-      config.before_configuration do
-        heroku_config = `heroku config --shell`.split("\n").inject({}) do |config, item|
-          key, value = item.split("=", 2)
-          config[key] = value
-          config
+
+      rake_tasks do
+        namespace :heroku_env do
+          namespace :config do
+            desc "Create heroku_env.yml from heroku config"
+            task :create => :environment do
+              heroku_config = `heroku config --shell`.split("\n").inject({}) do |config, item|
+                key, value = item.split(/\s+\=\>\s+/, 2)
+                config[key] = value
+                config
+              end
+              heroku_env = { :development => heroku_config.clone, :test => heroku_config.clone }
+              puts heroku_env.to_yaml
+            end
+          end
         end
-        custom_config = YAML.load_file(File.join(::Rails.root, "config", "heroku_env.yml"))[::Rails.env] rescue {}
-        config = heroku_config.merge(custom_config)
-        config.each do |key, value|
-          ENV[key] = value if !ENV.has_key?(key) && key != "BUNDLE_WITHOUT"
+      end
+
+      unless ENV.include?('HEROKU_UPID')
+        config.before_configuration do
+          if heroku_config = YAML.load_file(File.join(::Rails.root, "config", "heroku_env.yml"))[::Rails.env] rescue nil || {}
+            heroku_config.each do |key, value|
+              ENV[key] = value if !ENV.has_key?(key) && !["BUNDLE_WITHOUT", "RACK_ENV"].include?(key)
+            end
+          end
         end
       end
 
